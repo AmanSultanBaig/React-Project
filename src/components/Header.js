cc8358e95d6201d970263a484774dc40a7da4901
mac <mac@macs-MacBook-Pro.local>
2019-11-25 21:31:16 +0500

    some changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,10 +4,7 @@ class Header extends Component {
     render() {
         return (
             <ul>
-                <li><a href="default.asp">Home</a></li>
-                <li><a href="news.asp">News</a></li>
-                <li><a href="contact.asp">Contact</a></li>
-                <li><a href="about.asp">About</a></li>
+                <li><h1>Images List With ReactJS</h1></li>
             </ul>
 
         );
