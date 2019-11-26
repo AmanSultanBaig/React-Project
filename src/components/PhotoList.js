e5817d9e0a2f126e5173d656e79dc91793478172
mac <mac@macs-MacBook-Pro.local>
2019-11-26 20:04:06 +0500

    images are upladed in grid

--- src/components/PhotoList.js
+++ src/components/PhotoList.js
@@ -1,18 +1,20 @@
 import React, { Component } from 'react';
 
+// import images from '../images/';
+
 class PhotoList extends Component {
     render() {
         return (
             <div class="grid-container">
-                <div class="grid-item">1</div>
-                <div class="grid-item">2</div>
-                <div class="grid-item">3</div>
-                <div class="grid-item">4</div>
-                <div class="grid-item">5</div>
-                <div class="grid-item">6</div>
-                <div class="grid-item">7</div>
-                <div class="grid-item">8</div>
-                <div class="grid-item">9</div>
+                <div class="grid-item"><img src={require('../images/2.jpeg')}/></div>
+                <div class="grid-item"><img src={require('../images/3.jpeg')}/></div>
+                <div class="grid-item"><img src={require('../images/4.jpeg')}/></div>
+                <div class="grid-item"><img src={require('../images/6.jpg')}/></div>
+                <div class="grid-item"><img src={require('../images/5.jpg')}/></div>
+                <div class="grid-item"><img src={require('../images/7.jpg')}/></div>
+                <div class="grid-item"><img src={require('../images/images.jpeg')}/></div>
+                <div class="grid-item"><img src={require('../images/8.jpeg')}/></div>
+                <div class="grid-item"><img src={require('../images/9.jpg')}/></div>
             </div>
 
         );
