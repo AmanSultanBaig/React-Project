81cbeeff94bdc9a2565ab4e87cf06000d94fa4d3
mac <mac@macs-MacBook-Pro.local>
2019-11-26 20:18:45 +0500

    more css implemented

--- src/components/PhotoList.js
+++ src/components/PhotoList.js
@@ -3,18 +3,65 @@ import React, { Component } from 'react';
 // import images from '../images/';
 
 class PhotoList extends Component {
+    constructor(props) {
+        super(props);
+    }
     render() {
         return (
             <div class="grid-container">
-                <div class="grid-item"><img src={require('../images/2.jpeg')}/></div>
-                <div class="grid-item"><img src={require('../images/3.jpeg')}/></div>
-                <div class="grid-item"><img src={require('../images/4.jpeg')}/></div>
-                <div class="grid-item"><img src={require('../images/6.jpg')}/></div>
-                <div class="grid-item"><img src={require('../images/5.jpg')}/></div>
-                <div class="grid-item"><img src={require('../images/7.jpg')}/></div>
-                <div class="grid-item"><img src={require('../images/images.jpeg')}/></div>
-                <div class="grid-item"><img src={require('../images/8.jpeg')}/></div>
-                <div class="grid-item"><img src={require('../images/9.jpg')}/></div>
+                <div class="grid-item">
+                    <img src={require('../images/2.jpeg')} />
+                    <hr />
+                    <p>{this.props.description}</p>
+                </div>
+
+                <div class="grid-item">
+                    <img src={require('../images/3.jpeg')} />
+                    <hr />
+                    <p>{this.props.description}</p>
+                </div>
+
+                <div class="grid-item">
+                    <img src={require('../images/4.jpeg')} />
+                    <hr />
+                    <p>{this.props.description}</p>
+                </div>
+
+                <div class="grid-item">
+                    <img src={require('../images/6.jpg')} />
+                    <hr />
+                    <p>{this.props.description}</p>
+                </div>
+
+                <div class="grid-item">
+                    <img src={require('../images/5.jpg')} />
+                    <hr />
+                    <p>{this.props.description}</p>
+                </div>
+
+                <div class="grid-item">
+                    <img src={require('../images/7.jpg')} />
+                    <hr />
+                    <p>{this.props.description}</p>
+                </div>
+
+                <div class="grid-item">
+                    <img src={require('../images/images.jpeg')} />
+                    <hr />
+                    <p>{this.props.description}</p>
+                </div>
+
+                <div class="grid-item">
+                    <img src={require('../images/8.jpeg')} />
+                    <hr />
+                    <p>{this.props.description}</p>
+                </div>
+
+                <div class="grid-item">
+                    <img src={require('../images/9.jpg')} />
+                    <hr />
+                    <p>{this.props.description}</p>
+                </div>
             </div>
 
         );
